b9093b1b7c81258c21103590723309b27a30884c
Kaleb Clark <makersdigest@gmail.com>
2018-12-20 02:30:24 +0000

    Fixed..

--- arduino/foobar/foobar.ino
+++ arduino/foobar/foobar.ino
@@ -1,10 +1,12 @@
 /*
  * Maker's Digest
+ *
+ * <description>
  */
 void setup() {
 
   Serial.begin(19200);
-  Serial.println("Makers Digest: Ready")
+  Serial.println("Makers Digest: Ready");
 }
 
 void loop() {
